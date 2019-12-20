e8830f6cb6238392ca06dc62ec2829b070e728e2
basit96 <pakkobra007@gmail.com>
2019-12-20 17:04:33 +0500

    aashir + basit

--- app/src/main/java/com/example/all4cars/Profile.java
+++ app/src/main/java/com/example/all4cars/Profile.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +20,9 @@ import com.squareup.picasso.Picasso;
 public class Profile extends AppCompatActivity {
     ImageView imageView;
     TextView name,email;
+    String category;
+    LinearLayout LayoutAddService,LayoutViewService;
+    View view1,view2;
     DatabaseReference dref = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,10 @@ public class Profile extends AppCompatActivity {
         imageView =(ImageView) findViewById(R.id.profileImage);
         name=(TextView) findViewById(R.id.name);
         email=(TextView) findViewById(R.id.email);
+        LayoutAddService=(LinearLayout) findViewById(R.id.LayoutAddService);
+        LayoutViewService=(LinearLayout) findViewById(R.id.LayoutViewServices);
+        view1=(View) findViewById(R.id.view1);
+        view2=(View) findViewById(R.id.view2);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +53,15 @@ public class Profile extends AppCompatActivity {
                 name.setText(dataSnapshot.child("name").getValue().toString());
                 email.setText(dataSnapshot.child("Email").getValue().toString());
                 Picasso.get().load(dataSnapshot.child( "pic" ).getValue().toString()).into(imageView);
+                category=dataSnapshot.child("Category").getValue().toString();
+                if(category.equals("Customer")){
+
+                    LayoutAddService.setVisibility(View.GONE);
+                    LayoutViewService.setVisibility(View.GONE);
+                    view1.setVisibility(view1.GONE);
+                    view2.setVisibility(view2.GONE);
+
+                }
             }
 
             @Override
